a2c52bf1d380987ad63710d03d7f2b9c27c798db
arpitkumarsahu <aarpitkumarsahu99@gmail.com>
2023-02-28 14:34:01 +0530

    mock12 backend

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { connection } = require("./configs/db");
-
+const cors = require("cors");
 const {userRouter} = require("./routes/user.routes");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
